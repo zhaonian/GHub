55b55fc9bd539b49e5feaab297248615382b3c19
Zhaonian Luan <lzn0830@gmail.com>
2019-10-27 00:05:01 -0700

    commented out unfinished code and added TODO

--- app/src/main/java/io/zluan/ghub/di/auth/AuthScope.kt
+++ app/src/main/java/io/zluan/ghub/di/auth/AuthScope.kt
@@ -2,7 +2,7 @@ package io.zluan.ghub.di.auth
 
 import javax.inject.Scope
 
-/** A scope for login and registration. */
+/** A scope for login. */
 @Scope
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class AuthScope
--- app/src/main/java/io/zluan/ghub/network/auth/AuthService.kt
+++ app/src/main/java/io/zluan/ghub/network/auth/AuthService.kt
@@ -1,14 +1,15 @@
 package io.zluan.ghub.network.auth
 
+import io.zluan.ghub.network.ApiResponse
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.Field
 
 /**
- * TODO(zluan): looks like there are 2 ways to authenticate, plain username/password vs. OAuth
+ * TODO(zluan): need to figure out how OAuth works.
  */
 interface AuthService {
-    suspend fun login(
-        @Field("username") username: String,
-        @Field("password") password: String
-    ): Flow
+//    suspend fun login(
+//        @Field("username") username: String,
+//        @Field("password") password: String
+//    ): Flow<ApiResponse<LoginResponse>>
 }

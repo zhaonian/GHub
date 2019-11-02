95924ed3c16478973739c0deffd5d2e62191987a
Zhaonian Luan <lzn0830@gmail.com>
2019-11-02 13:32:06 -0700

    Add dark mode
    rename nav graph and some clean up
    
    add activity navigation
    
    add kdoc for MainActivity
    
    change current style to MaterialComponents
    
    upgrade material to get dayNight theme
    
    daynight
    
    add dark mode
    
    replace switch with material switch

--- app/src/main/java/io/zluan/ghub/ui/main/MainActivity.kt
+++ app/src/main/java/io/zluan/ghub/ui/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package io.zluan.ghub.ui.main
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -12,8 +11,13 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 import io.zluan.ghub.R
+import io.zluan.ghub.util.ThemeUtil
+import timber.log.Timber
 
+/** The one and only Activity that contains all the fragments related to Github interaction. */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -26,8 +30,11 @@ class MainActivity : AppCompatActivity() {
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "maybe shows a circular menu with options when pressed", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            Snackbar.make(
+                view,
+                "Show a circular menu with options when pressed",
+                Snackbar.LENGTH_LONG
+            ).setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -36,11 +43,20 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
             R.id.nav_home,
-            R.id.nav_about,
-            R.id.nav_settings
+            R.id.nav_settings,
+            R.id.nav_about
         ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        Timber.plant(Timber.DebugTree())
+        (navView.menu.findItem(R.id.nav_dark_mode).actionView as SwitchMaterial)
+            .setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    ThemeUtil.applyTheme(ThemeUtil.DARK_MODE)
+                } else {
+                    ThemeUtil.applyTheme(ThemeUtil.DEFAULT_MODE)
+                }
+            }
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/io/zluan/ghub/util/ThemeUtil.kt
@@ -0,0 +1,34 @@
+package io.zluan.ghub.util
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
+
+/** Utils for setting a theme. */
+object ThemeUtil {
+    /** Enforce light mode. */
+    const val LIGHT_MODE = "light"
+    /** Enforce dark mode. */
+    const val DARK_MODE = "dark"
+    /**
+     * Follows the system settings.
+     * For >= API 28, there is a system-wide dark mode that can be toggled on by the user. Turning
+     * on battery saver will also trigger dark mode.
+     * For <  API 28, simply follow the battery saver's setting.
+     */
+    const val DEFAULT_MODE = "default"
+
+    /** Apply the selected theme. */
+    fun applyTheme(theme: String) {
+        val mode = when (theme) {
+            LIGHT_MODE -> AppCompatDelegate.MODE_NIGHT_NO
+            DARK_MODE -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> {
+                when {
+                    Build.VERSION.SDK_INT >= 28 -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    else -> AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                }
+            }
+        }
+        AppCompatDelegate.setDefaultNightMode(mode)
+    }
+}

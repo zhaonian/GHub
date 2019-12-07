0b1db41b1a8f7edf35a35fc0ce0376858df896a2
Zhaonian Luan <lzn0830@gmail.com>
2019-12-06 23:32:20 -0800

    provide apollo client via Dagger

--- app/src/main/java/io/zluan/ghub/network/Constants.kt
+++ app/src/main/java/io/zluan/ghub/network/Constants.kt
@@ -3,5 +3,6 @@ package io.zluan.ghub.network
 class Constants {
     companion object {
         const val GITHUB_BASE_URL = "https://api.github.com"
+        const val GITHUB_GRAPHQL_BASE_URL = "https://api.github.com/graphql"
     }
 }

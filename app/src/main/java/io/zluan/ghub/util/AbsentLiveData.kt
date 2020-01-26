59442646aaedc2d0d6ec3a73bd04b6ad5951f565
Zhaonian Luan <lzn0830@gmail.com>
2020-01-25 22:48:20 -0800

    add infra for MVI archtiecture

---
+++ app/src/main/java/io/zluan/ghub/util/AbsentLiveData.kt
@@ -0,0 +1,21 @@
+package io.zluan.ghub.util
+
+import androidx.lifecycle.LiveData
+
+/**
+ * A LiveData class that has [null] value.
+ *
+ * Copied from https://github.com/android/architecture-components-samples/blob/master/GithubBrowserSample/app/src/main/java/com/android/example/github/util/AbsentLiveData.kt
+ */
+class AbsentLiveData<T : Any?> private constructor(): LiveData<T>() {
+    init {
+        // use post instead of set since this can be created on any thread
+        postValue(null)
+    }
+
+    companion object {
+        fun <T> create(): LiveData<T> {
+            return AbsentLiveData()
+        }
+    }
+}
